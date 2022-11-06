31816564a477a13401ca540a4e678babd4eff67e
Alexandre WEISSER <alexandre.weisser@gmail.com>
2022-11-06 11:27:14 +0100

    grammar

--- ihm/src/UI/common/Footer.js
+++ ihm/src/UI/common/Footer.js
@@ -15,9 +15,13 @@ export default function Footer() {
                 flexWrap: "wrap"
             }}>
                 <p>
-                    Ce site web n'est là que pour nous "faciliter" la vie dans le processus de création d'une douzaine de produits à base de testicules.
+                    Ce site web n'est là que pour nous "faciliter" la vie dans le processus de création d'une douzaine de produits à base de testiculess et surtout vous faire rire ! On ne peut pas y dépenser d'argent (mais dieu sait que ça nous a coûté à réaliser).
                     <br/>
-                    On ne peut pas dépenser d'argent ici (mais le matériel coûte cher).
+                    Si êtes chaud.e.s à chopper un objet pour la postérité, vous pouvez le faire en créant un panier ici.
+                    <br/>
+                    Le login ne sert qu'à lier la panier à un nom pour pas qu'on se perde. Si vous êtes content.e de la prestation, validez votre panier et laissez un commentaire !
+                    <br/>
+                    <code>xoxo Al² Studio</code>
                 </p>
             </Paper>
         </footer>
